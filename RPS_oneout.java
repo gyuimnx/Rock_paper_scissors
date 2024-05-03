0f0758ab0812022a5f3008d3650b18040ba2a19c
MinGyu Lim <alsrb2277@naver.com>
2024-05-03 19:01:18 +0900

    가위, 바위, 보 이외의 것을 입력하면 시스템 종료되도록 수정

--- RPS_oneout.java
+++ RPS_oneout.java
@@ -9,36 +9,47 @@ public class RPS_oneout {
 			String my_first_pick = s.next();
 			System.out.println("나의 두번째 선택: ");
 			String my_second_pick = s.next();
-
-			String[] hands1 = {"가위", "바위", "보"};
-			int pick1 = (int) (Math.random() * 3);
-			String computer_pick1 = hands1[pick1];
-			System.out.println("컴퓨터의 첫번째 선택: "+computer_pick1);
 			
-			int pick2 = (int) (Math.random() * 3);
-			String computer_pick2 = hands1[pick2];
-			System.out.println("컴퓨터의 두번째 선택: "+computer_pick2);
-
-			System.out.println("나의 최종 선택(" + my_first_pick + " or " + my_second_pick + "): ");
-			String my_final_pick = s.next();
-			
-			String[] hands2 = {computer_pick1, computer_pick2};
-			int pick3 = (int) (Math.random() * 2);
-			String computer_final_pick = hands2[pick3];
-			System.out.println("컴퓨터의 최종 선택: "+computer_final_pick);
-
-			if (my_final_pick.equals(computer_final_pick)) {
-				System.out.println("비겼네요. 다시 준비하세요!");
-			}else if
-			((my_final_pick.equals("가위") && computer_final_pick.equals("보")) || 
-			(my_final_pick.equals("바위")&&computer_final_pick.equals("가위")) || 
-			(my_final_pick.equals("보")&&computer_final_pick.equals("바위"))) {
-				System.out.println("이겼습니다.");
+			if (!my_first_pick.equals("가위") && !my_first_pick.equals("바위") && !my_first_pick.equals("보")
+					|| !my_second_pick.equals("가위") && !my_second_pick.equals("바위") && !my_second_pick.equals("보")) {
+				System.out.println("가위, 바위, 보 중에 선택하세요.");
 				break;
 			}else {
-				System.out.println("졌습니다.");
-				break;
-			}
+				String[] hands1 = {"가위", "바위", "보"};
+				int pick1 = (int) (Math.random() * 3);
+				String computer_pick1 = hands1[pick1];
+				System.out.println("컴퓨터의 첫번째 선택: "+computer_pick1);
+				
+				int pick2 = (int) (Math.random() * 3);
+				String computer_pick2 = hands1[pick2];
+				System.out.println("컴퓨터의 두번째 선택: "+computer_pick2);
+	
+				System.out.println("나의 최종 선택(" + my_first_pick + " or " + my_second_pick + "): ");
+				String my_final_pick = s.next();
+				
+				if (!my_final_pick.equals(my_first_pick) && !my_final_pick.equals(my_second_pick)) {
+					System.out.println("첫번째 선택과 두번째 선택 중에 고르세요.");
+					break;
+				}else {
+					String[] hands2 = {computer_pick1, computer_pick2};
+					int pick3 = (int) (Math.random() * 2);
+					String computer_final_pick = hands2[pick3];
+					System.out.println("컴퓨터의 최종 선택: "+computer_final_pick);
+		
+					if (my_final_pick.equals(computer_final_pick)) {
+						System.out.println("비겼네요. 다시 준비하세요!");
+					}else if
+					((my_final_pick.equals("가위") && computer_final_pick.equals("보")) || 
+					(my_final_pick.equals("바위")&&computer_final_pick.equals("가위")) || 
+					(my_final_pick.equals("보")&&computer_final_pick.equals("바위"))) {
+						System.out.println("당신이 이겼습니다.");
+						break;
+					}else {
+						System.out.println("당신이 졌습니다.");
+						break;
+						}
+					}
+				}
 		} while (true);
 //			
 		s.close();
