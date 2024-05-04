e9fe17b7bc0184567d7054982a2ae71068fb94a8
MinGyu Lim <alsrb2277@naver.com>
2024-05-04 12:35:08 +0900

    일반 가위바위보에서도 바르게 입력하지 않으면 다시 실행하도록 수정

--- RPS.java
+++ RPS.java
@@ -10,23 +10,27 @@ public class RPS {
 			System.out.println("안 내면 진다, 가위 바위 보!: ");
 			my_pick = s.next();
 			
-			String[] hands = {"가위", "바위", "보"};
-			int pick = (int) (Math.random() * 3);
-			computer_pick = hands[pick];
-			
-			System.out.println("컴퓨터: "+computer_pick);
-			
-			if (my_pick.equals(computer_pick)) {
-				System.out.println("비겼네요. 다시 준비하세요!");
-			}else if
-			((my_pick.equals("가위") && computer_pick.equals("보")) || 
-			(my_pick.equals("바위")&&computer_pick.equals("가위")) || 
-			(my_pick.equals("보")&&computer_pick.equals("바위"))) {
-				System.out.println("이겼습니다.");
-				break;
-			}else {
-				System.out.println("졌습니다.");
-				break;
+			if (!my_pick.equals("가위") && !my_pick.equals("바위") && !my_pick.equals("보")) {
+				System.out.println("가위, 바위, 보 중에 선택하세요.");
+			} else {
+				String[] hands = {"가위", "바위", "보"};
+				int pick = (int) (Math.random() * 3);
+				computer_pick = hands[pick];
+				
+				System.out.println("컴퓨터: "+computer_pick);
+				
+				if (my_pick.equals(computer_pick)) {
+					System.out.println("비겼네요. 다시 준비하세요!");
+				}else if
+				((my_pick.equals("가위") && computer_pick.equals("보")) || 
+				(my_pick.equals("바위")&&computer_pick.equals("가위")) || 
+				(my_pick.equals("보")&&computer_pick.equals("바위"))) {
+					System.out.println("이겼습니다.");
+					break;
+				}else {
+					System.out.println("졌습니다.");
+					break;
+				}
 			}
 		} while (true);
 			
